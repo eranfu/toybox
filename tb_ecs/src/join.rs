897d227d06e76b9ca3419d3b48f0b11eb47d8694
mingyuan-f <1119369173@qq.com>
2020-11-24 12:09:53 +0800

    impl join tuple

--- tb_ecs/src/join.rs
+++ tb_ecs/src/join.rs
@@ -1,4 +1,4 @@
-use hibitset::{BitIter, BitSet, BitSetLike};
+use hibitset::{BitIter, BitSet, BitSetAnd, BitSetLike};
 
 use tb_core::Id;
 use tb_storage::StorageItems;
@@ -19,7 +19,7 @@ trait Join: Sized {
         }
     }
     fn open(self) -> (Self::BitSet, Self::Components);
-    fn get(components: &mut Self::Components, id: Id) -> Self::Component;
+    unsafe fn get(components: &mut Self::Components, id: Id) -> Self::Component;
 }
 
 struct JoinIterator<J: Join> {
@@ -33,7 +33,7 @@ impl<J: Join> Iterator for JoinIterator<J> {
     fn next(&mut self) -> Option<Self::Item> {
         self.mask_iter
             .next()
-            .map(|id| J::get(&mut self.components, id.into()))
+            .map(|id| unsafe { J::get(&mut self.components, id.into()) })
     }
 }
 
@@ -46,8 +46,8 @@ impl<'r, C: Component, R: ReadOrder> Join for &'r ReadComponents<'r, C, R> {
         self.components.storage.open()
     }
 
-    fn get(components: &mut Self::Components, id: Id) -> Self::Component {
-        unsafe { components.get(id) }
+    unsafe fn get(components: &mut Self::Components, id: Id) -> Self::Component {
+        components.get(id)
     }
 }
 
@@ -60,8 +60,46 @@ impl<'r, C: Component> Join for &'r mut WriteComponents<'r, C> {
         self.components.storage.open_mut()
     }
 
-    fn get(components: &mut Self::Components, id: Id) -> Self::Component {
+    unsafe fn get(components: &mut Self::Components, id: Id) -> Self::Component {
         let components: *mut Self::Components = components as *mut Self::Components;
-        unsafe { (*components).get_mut(id) }
+        (*components).get_mut(id)
     }
 }
+
+macro_rules! bit_set_and {
+    ($b:ty) => { $b };
+    ($b0:ty, $($b1:ty), +) => {
+        BitSetAnd<$b0, bit_set_and!($($b1), +)>
+    };
+    ($b:expr) => { $b };
+    ($b0:expr, $($b1:expr), +) => {
+        BitSetAnd($b0, bit_set_and!($($b1), +))
+    };
+}
+
+macro_rules! impl_join_tuple {
+    ($j:ident) => {};
+    ($j0:ident, $($j1:ident), +) => {
+        impl_join_tuple!($($j1), +);
+        impl<$j0: Join, $($j1: Join), +> Join for ($j0, $($j1), +) {
+            type BitSet = bit_set_and!($j0::BitSet, $($j1::BitSet), +);
+            type Component = ($j0::Component, $($j1::Component), +);
+            type Components = ($j0::Components, $($j1::Components), +);
+
+            #[allow(non_snake_case)]
+            fn open(self) -> (Self::BitSet, Self::Components) {
+                let ($j0, $($j1), +) = self;
+                let ($j0, $($j1), +) = ($j0.open(), $($j1.open()), +);
+                (bit_set_and!($j0.0, $($j1.0), +), ($j0.1, $($j1.1), +))
+            }
+
+            #[allow(non_snake_case)]
+            unsafe fn get(components: &mut Self::Components, id: Id) -> Self::Component {
+                let ($j0, $($j1), +) = components;
+                ($j0::get($j0, id), $($j1::get($j1, id)), +)
+            }
+        }
+    };
+}
+
+impl_join_tuple!(J0, J1, J2, J3, J4, J5, J6, J7);
