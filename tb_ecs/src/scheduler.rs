b3836ec56080d1927b6a397a334018780a85396d
dahai-f <1119369173@qq.com>
2021-05-06 08:53:40 +0800

    schedule run systems

--- tb_ecs/src/scheduler.rs
+++ tb_ecs/src/scheduler.rs
@@ -1,4 +1,6 @@
+use std::cell::UnsafeCell;
 use std::collections::HashMap;
+use std::ops::DerefMut;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
 use dashmap::DashSet;
@@ -11,7 +13,7 @@ use crate::{ResourcesChangeEvent, System, SystemData, SystemInfo, SystemRegistry
 
 pub struct Scheduler {
     resources_change_event_reader: ReaderHandle,
-    systems: Vec<Box<dyn RunnableSystem>>,
+    systems: Vec<RunnableCell>,
     dependants: Vec<DashSet<usize>>,
     dependencies_counter_cache: Vec<AtomicUsize>,
     dependencies_counter: Vec<AtomicUsize>,
@@ -37,29 +39,33 @@ impl Scheduler {
         if events.read_any(&mut self.resources_change_event_reader) {
             self.refresh_systems(world);
         }
-        self.dependencies_counter_cache
-            .par_iter()
-            .enumerate()
-            .for_each(|(i, counter): (usize, &AtomicUsize)| {
-                self.dependencies_counter[i]
-                    .store(counter.load(Ordering::Relaxed), Ordering::Relaxed)
-            });
 
-        self.dependencies_counter
-            .par_iter()
-            .enumerate()
-            .for_each(|(i, counter): (usize, &AtomicUsize)| {})
+        self.dependencies_counter.par_iter().enumerate().for_each(
+            |(i, counter): (usize, &AtomicUsize)| {
+                counter.store(
+                    self.dependencies_counter_cache[i].load(Ordering::Relaxed),
+                    Ordering::Relaxed,
+                );
+            },
+        );
+
+        (0..self.systems.len()).into_par_iter().for_each(|i| {
+            self.run_system_recursive(i, world);
+        })
+    }
+
+    fn run_system_recursive(&self, i: usize, world: &World) {
+        let counter = &self.dependencies_counter[i];
+        if counter.fetch_sub(1, Ordering::Release) == 1 {
+            counter.load(Ordering::Acquire);
+            self.systems[i].get_mut().run(world);
+            self.dependants[i].par_iter().for_each(|dependant| {
+                self.run_system_recursive(*dependant, world);
+            })
+        }
     }
 
     fn refresh_systems(&mut self, world: &mut World) {
-        self.systems.clear();
-        self.dependencies_counter_cache
-            .par_iter_mut()
-            .for_each(|counter: &mut AtomicUsize| counter.store(0, Ordering::Relaxed));
-        self.dependants
-            .par_iter_mut()
-            .for_each(|dependant| dependant.clear());
-
         let mut sr = SystemRegistry::get_instance();
         let sr: &mut SystemRegistry = &mut sr;
         let systems = sr.systems();
@@ -69,20 +75,28 @@ impl Scheduler {
             .collect();
 
         let mut info_to_index = HashMap::with_capacity(infos.len());
+        self.systems.clear();
         self.systems.reserve(infos.len());
         for (i, (&info, _node)) in infos.iter().enumerate() {
-            self.systems.push(info.create_system());
+            self.systems
+                .push(RunnableCell(UnsafeCell::new(info.create_system())));
             info_to_index.insert(info, i);
         }
 
-        self.dependencies_counter_cache
-            .resize_with(self.systems.len(), || AtomicUsize::new(0));
+        self.dependants
+            .par_iter_mut()
+            .for_each(|dependant| dependant.clear());
         self.dependants.resize(self.systems.len(), DashSet::new());
         infos
             .par_iter()
             .enumerate()
             .for_each(|(i, (_info, node))| self.add_dependants(i, node, &info_to_index, systems));
 
+        self.dependencies_counter_cache
+            .par_iter()
+            .for_each(|counter: &AtomicUsize| counter.store(1, Ordering::Relaxed));
+        self.dependencies_counter_cache
+            .resize_with(self.systems.len(), || AtomicUsize::new(1));
         self.dependants
             .par_iter()
             .for_each(|dependants: &DashSet<usize>| {
@@ -93,7 +107,7 @@ impl Scheduler {
 
         self.dependencies_counter
             .resize_with(self.dependencies_counter_cache.len(), || {
-                AtomicUsize::new(0)
+                AtomicUsize::new(1)
             });
     }
 
@@ -116,6 +130,17 @@ impl Scheduler {
             });
     }
 }
+
+struct RunnableCell(UnsafeCell<Box<dyn RunnableSystem>>);
+
+impl RunnableCell {
+    #[allow(clippy::mut_from_ref)]
+    pub(crate) fn get_mut(&self) -> &mut dyn RunnableSystem {
+        (unsafe { &mut *self.0.get() }).deref_mut()
+    }
+}
+
+unsafe impl Sync for RunnableCell {}
 
 pub trait RunnableSystem: Send + Sync {
     fn run(&mut self, world: &World);
